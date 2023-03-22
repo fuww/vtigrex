550eedbe1870dc11da605fbdab8a1508f207b6b4
Péter Pál Koszta <peterpal@fashionunited.com>
2023-03-22 13:21:02 +0100

    Initial commit

---
+++ lib/vtigrex.ex
@@ -0,0 +1,18 @@
+defmodule Vtigrex do
+  @moduledoc """
+  Documentation for `Vtigrex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vtigrex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vtigrex_test.exs
@@ -0,0 +1,8 @@
+defmodule VtigrexTest do
+  use ExUnit.Case
+  doctest Vtigrex
+
+  test "greets the world" do
+    assert Vtigrex.hello() == :world
+  end
+end
