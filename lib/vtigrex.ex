86c475d9396d038a9688291363212d3c06d7b5b1
Péter Pál Koszta <peterpal@fashionunited.com>
2023-03-22 15:46:37 +0100

    Get current user with /me

--- lib/vtigrex.ex
+++ lib/vtigrex.ex
@@ -1,18 +1,56 @@
 defmodule Vtigrex do
   @moduledoc """
-  Documentation for `Vtigrex`.
+  vtiger Rest API wrapper
   """
 
   @doc """
-  Hello world.
+  Initializes the client for your vtiger API.
 
   ## Examples
 
-      iex> Vtigrex.hello()
-      :world
+      iex> Vtigrex.client(
+      iex>   "https://your_instance.odx.vtiger.com/restapi/v1/vtiger/default",
+      iex>   "your_user@your_host.com",
+      iex>   "y0ur4cc3sk3y"
+      iex>)
+      %Tesla.Client{
+        pre: [
+          {Tesla.Middleware.BaseUrl, :call, ["https://your_instance.odx.vtiger.com/restapi/v1/vtiger/default"]},
+          {Tesla.Middleware.BasicAuth, :call, [[username: "your_user@your_host.com", password: "y0ur4cc3sk3y"]]},
+          {Tesla.Middleware.JSON, :call, [[{:decode_content_types, ["text/json"]}]]}
+        ]
+      }
 
   """
-  def hello do
-    :world
+  @spec client(String.t(), String.t(), String.t()) ::
+          Tesla.Client.t()
+  def client(base_url, username, access_key) do
+    middlewares = [
+      {Tesla.Middleware.BaseUrl, base_url},
+      {Tesla.Middleware.BasicAuth, username: username, password: access_key},
+      {Tesla.Middleware.JSON, decode_content_types: ["text/json"]}
+    ]
+
+    Tesla.client(middlewares)
+  end
+
+  @doc """
+  Gets the current user.
+  """
+  @spec me(Tesla.Client.t()) :: {:ok, map()} | {:error, String.t()}
+  def me(client) do
+    client
+    |> Tesla.get("/me")
+    |> parse_result()
+  end
+
+  defp parse_result(
+         {:ok, %Tesla.Env{status: 200, body: %{"success" => true, "result" => %{} = result}}}
+       ) do
+    {:ok, result}
+  end
+
+  defp parse_result({:ok, %Tesla.Env{status: 401}}) do
+    {:error, "Unauthorized"}
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,2 @@
 ExUnit.start()
+Application.put_env(:tesla, :adapter, Tesla.Mock)
